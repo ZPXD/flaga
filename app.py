89af1ab4bc5eb1416d27b73b1dd8a0d79419d2ed
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-02-02 18:46:56 +0100

    Update app.py
    kubus puchatek

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app=Flask(__name__)
 def index():
     text = open('xd.txt').read()
     return render_template("index.html", text=text)
+
+@app.route('/kubus_puchatek')
+def kubus_puchatek():
+    return render_template("kubus_puchatek.html")
     
 if __name__=="__main__":
     app.run()
