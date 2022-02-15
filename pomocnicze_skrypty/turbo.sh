bdb7aa7f7a929a86eae6b01eba1d7124e0552949
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-02-15 11:34:43 +0100

    Update turbo.sh

--- pomocnicze_skrypty/turbo.sh
+++ pomocnicze_skrypty/turbo.sh
@@ -3,18 +3,6 @@
 # Domena.
 domena=$1
 
-# Przygotowanie katalogu /var/www na flage.
-mkdir /var/www
-cd /var
-sudo chown -R www-data:www-data /var/www
-sudo chmod -R 775 /var/www
-cd /var/www
-
-# Pobieramy git a nim repozytorium flaga i wchodzimy do srodka.
-sudo apt install git
-git clone https://github.com/ZPXD/flaga.git
-cd /var/www/flaga
-
 # Dogranie paczek.
 sudo python3 /var/www/flaga/pomocnicze_skrypty/xD.py
 
@@ -26,4 +14,3 @@ pip3 install -r requirements.txt
 
 # Serwerowe pliki i uruchomienie usługi i serwera.
 sudo python3 /var/www/flaga/pomocnicze_skrypty/xd.py $domena
-
