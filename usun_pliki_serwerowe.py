6954a01a7bcff3b0db9ae4075d94655bd08c6ff2
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-02-15 05:40:22 +0100

    Update usun_pliki_serwerowe.py

--- usun_pliki_serwerowe.py
+++ usun_pliki_serwerowe.py
@@ -11,7 +11,7 @@ def clear_files_and_folders_creation():
         nginx_file_2 = '/etc/nginx/sites-enabled/{}'
         nginx_file_2 = nginx_file_2.format(domena)
         os.system('sudo rm {}'.format(nginx_file_1))
-        print('Plik nginx 1 usunięty:', nginx_file_1, '\n')
+        print('Plik nginx 1 usuniety:', nginx_file_1, '\n')
     except:
         pass
 
@@ -20,7 +20,7 @@ def clear_files_and_folders_creation():
     nginx_file_2 = nginx_file_2.format(domena)
     try:
         os.system('sudo rm {}'.format(nginx_file_2))
-        print('Plik nginx 2 usunięty:', nginx_file_2, '\n')
+        print('Plik nginx 2 usuniety:', nginx_file_2, '\n')
     except:
         pass
 
@@ -28,7 +28,7 @@ def clear_files_and_folders_creation():
     gunicorn_file = '/etc/systemd/system/flaga.service'
     try:
         os.system('sudo rm {}'.format(gunicorn_file))
-        print('Plik gunicorn usunięty:', gunicorn_file, '\n')
+        print('Plik gunicorn usuniety:', gunicorn_file, '\n')
     except:
         pass
 
@@ -38,5 +38,11 @@ if __name__ == '__main__':
     domena = config['XD']['domena']
     if len(sys.argv) > 1:
         domena = sys.argv[1]
-    print('Podana przez Ciebie domena na której stawiamy stronkę to:\n', domena, '\n')
-    clear_files_and_folders_creation(domena)
+    if domena == 'xd.pl':
+        print('Nie zmieniles domeny. Zrob to piszac:\ndomena=nazwa-TWOJEJ-domeny.pl')
+    else:
+        print('Podana przez Ciebie domena na ktorej postawilismy stronke to:\n', domena, '\n')
+        print('Sprawdz czy sie zgadza i edytuj jej nazwę piszac:\ndomena=nazwa-TWOJEJ-domeny.pl')
+        print('Jak nazwa domeny bedzie sie zgadzac, uruchom spowrotem plik xd.py:\nsudo python3 xd.py $domena')
+        print('Powinno się udac. W przeciwnym razie napsz na dziale pomoc.')
+        clear_files_and_folders_creation(domena)
