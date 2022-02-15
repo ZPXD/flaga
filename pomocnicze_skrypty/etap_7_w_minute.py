c3791f298e2dd42fe88c2def5eec7c31fcde2caa
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-02-15 10:28:16 +0100

    Update etap_7_w_minute.py

--- pomocnicze_skrypty/etap_7_w_minute.py
+++ pomocnicze_skrypty/etap_7_w_minute.py
@@ -3,16 +3,16 @@ import os
 import sys
 
 def etap_7_w_minute(domena):
-    os.system('mkdir /var/www')
-    os.system('chown -R www-data:www-data /var/www')
-    os.system('chmod -R 775 /var/www')
-    os.system('apt install git')
+    os.system('sudo mkdir /var/www')
+    os.system('sudo chown -R www-data:www-data /var/www')
+    os.system('sudo chmod -R 775 /var/www')
+    os.system('sudo apt install git')
     os.system('git clone https://github.com/ZPXD/flaga.git /var/www')
-    os.system('python3 /var/www/flaga/pomocnicze_skrypty/xD.py')
-    os.system('python3 -m venv /var/www/flaga/flagaenv')
+    os.system('sudo python3 /var/www/flaga/pomocnicze_skrypty/xD.py')
+    os.system('sudo python3 -m venv /var/www/flaga/flagaenv')
     os.system('source /var/www/flaga/flagaenv/bin/activate')
-    os.system('pip3 install -r /var/www/flaga/requirements.txt')
-    os.system('python3 /var/www/flaga/pomocnicze_skrypty/xd.py $domena')
+    os.system('sudo pip3 install -r /var/www/flaga/requirements.txt')
+    #os.system('python3 /var/www/flaga/pomocnicze_skrypty/xd.py $domena')
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
