56d08460b6f3c0e208c65fd67f3d08015b557292
ZPXD <98432112+ZPXD@users.noreply.github.com>
2022-02-19 02:42:24 +0100

    Update xD.py

--- pomocnicze_skrypty/xD.py
+++ pomocnicze_skrypty/xD.py
@@ -1,7 +1,7 @@
 
 import os
 
-os.system('sudo apt install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools')
+os.system('sudo apt install python3-pip python3-dev python3-venv build-essential libssl-dev libffi-dev python3-setuptools --yes')
 os.system('sudo apt install nano --yes')
 os.system('sudo apt remove apache2 --yes')
 os.system('sudo apt install nginx --yes')
