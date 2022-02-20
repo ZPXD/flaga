d68ffe0e88c393027cf242e1bafb718e8adecf1a
PhotoVolter <photovolter@gmail.com>
2022-02-20 20:13:22 +0000

    wersja działająca

--- pomocnicze_skrypty/create_swapfile_on_aws_ec2_server.sh
+++ pomocnicze_skrypty/create_swapfile_on_aws_ec2_server.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
+arg1=$1
+do_the_job='mkswapfile'
+# Zmienna size określa wielkość pliku wymiany
+# size=2G
+size=1G
 
-do_the_job=$1
+#echo "\$0 to: $0"
+#echo "\$1 to: $1"
+#echo "\$2 to: $2"
 
-swapfile = "/swapfile"
+swapfile="/swapfile"
 
-if [ -e $swapfile ]; then
+if [[ -e $swapfile ]]; then
 	echo "$swapfile exists!"
 # Checking the swapfile is non-existing
-elif [ $1 == "mkswapfile" ]; then
+elif [[ $do_the_job == $arg1 ]]; then
 	echo "Tworzę swapfile w pliku $swapfile"
-# 	if [ -z $(swapon --show=NAME) ]; then
-#     		fallocate -l 2G /swapfile && 
-# 		chmod 600 $swapfile &&
-# 		mkswap $swapfile
-# 		swapon $swapfile
-# 		echo -e "$swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab
-# 	fi
+ 	if [[ -z $(swapon --show=NAME) ]]; then
+     		fallocate -l $size /swapfile && 
+ 		chmod 600 $swapfile &&
+ 		mkswap $swapfile
+ 		swapon $swapfile
+		if [[ -z $(grep 'swap' < /etc/fstab) ]]; then
+ 			echo -e "$swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab
+		else
+			echo "Wpis odnoszący się do swap już istnieje w /etc/fstab. Skontroluj zawartość /etc/fstab i wyedytuj go samodzielnie."
+		fi
+ 	fi
 else
-	if [ -z $(swapon --show=NAME) ]; then
-    		echo "fallocate -l 2G /swapfile" &&
+	if [[ -z $(swapon --show=NAME) ]]; then
+    		echo "Jeżeli wywołasz skrypt z argumentem $do_the_job, to wykonam polecenia:" &&
+    		echo "fallocate -l $size /swapfile" &&
 		echo "chmod 600 $swapfile" &&
 		echo "mkswap $swapfile"
 		echo "swapon $swapfile"
