8f0f35d27298e50a1c39703d58a5a6c9696fbc98
photovolter <photovolter@gmail.com>
2022-02-20 23:58:52 +0100

    added some comments to script

--- pomocnicze_skrypty/create_swapfile_on_aws_ec2_server.sh
+++ pomocnicze_skrypty/create_swapfile_on_aws_ec2_server.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
+##############################################################################
+# # Bash (watch out double square brackets!) script                          #
+# # for create swapfile on AWS EC2 Ubuntu server                             #
+# # Tested on Ubuntu 20.04 LTS                                               #
+##############################################################################
+
 arg1=$1
 do_the_job='mkswapfile'
-# Zmienna size określa wielkość pliku wymiany
-# size=2G
+##############################################################################
+# # Zmienna size określa wielkość pliku wymiany # size is swapfile size :)   #
+# # size=2G                                                                  #
+##############################################################################
 size=1G
-
-#echo "\$0 to: $0"
-#echo "\$1 to: $1"
-#echo "\$2 to: $2"
+##############################################################################
+# # For debugging purpose                                                    #
+# #echo "\$0 to: $0"                                                         #
+# #echo "\$1 to: $1"                                                         #
+# #echo "\$2 to: $2"                                                         #
+##############################################################################
 
 swapfile="/swapfile"
 
+##############################################################################
+# # Checking the swapfile is non-existing # I'm sorry for my english :)      #
+##############################################################################
 if [[ -e $swapfile ]]; then
 	echo "$swapfile exists!"
-# Checking the swapfile is non-existing
+##############################################################################
+## If swapfile is non exist, then check the argument1 is proper              #
+## For now only accepted argument is 'mkswapfile' -                          #
+## empty argument or any other is equivalent of help (it shows commands      #
+## queue)                                                                    #
+##############################################################################
 elif [[ $do_the_job == $arg1 ]]; then
 	echo "Tworzę swapfile w pliku $swapfile"
  	if [[ -z $(swapon --show=NAME) ]]; then
@@ -27,6 +45,11 @@ elif [[ $do_the_job == $arg1 ]]; then
 			echo "Wpis odnoszący się do swap już istnieje w /etc/fstab. Skontroluj zawartość /etc/fstab i wyedytuj go samodzielnie."
 		fi
  	fi
+##############################################################################
+# #                                                                          #
+# # If argument1 is not valid show command queue                             #
+# #                                                                          #
+##############################################################################
 else
 	if [[ -z $(swapon --show=NAME) ]]; then
     		echo "Jeżeli wywołasz skrypt z argumentem $do_the_job, to wykonam polecenia:" &&
@@ -37,5 +60,9 @@ else
 		echo "echo -e "$swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab"
 	fi
 fi
-
+##############################################################################
+# #                                                                          #
+# # Always show "swapon --show"                                              #
+# #                                                                          #
+##############################################################################
 swapon --show
