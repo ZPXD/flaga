205af0b53bee6bf20a39322b4f21456ff6788f82
photovolter <photovolter@gmail.com>
2022-02-20 17:30:05 +0100

    dodany skrypt tworzący swapfile na AWS EC2

---
+++ pomocnicze_skrypty/create_swapfile_on_aws_ec2_server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+do_the_job=$1
+
+swapfile = "/swapfile"
+
+if [ ! -e $swapfile ]; then
+	echo "$swapfile exists!"
+# Checking the swapfile is non-existing
+# else:
+# 	if [ -z $(swapon --show=NAME) ]; then
+#     		fallocate -l 2G /swapfile && 
+# 		chmod 600 $swapfile &&
+# 		mkswap $swapfile
+# 		swapon $swapfile
+# 		echo -e "$swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab
+# 	fi
+# fi
+else:
+	if [ -z $(swapon --show=NAME) ]; then
+    		echo "fallocate -l 2G /swapfile" &&
+		echo "chmod 600 $swapfile" &&
+		echo "mkswap $swapfile"
+		echo "swapon $swapfile"
+		echo "echo -e "$swapfile\tswap\tswap\tdefaults\t0\t0" >> /etc/fstab"
+	fi
+fi
+swapon --show
+    
